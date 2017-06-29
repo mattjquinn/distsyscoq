95acb23e6527da109b6cf538c96e70e833b47fc8
Matt Quinn <matt@mattjquinn.com>
2017-06-29 14:37:39 +0300

    Up to but excluding reg_exp_of_list exercise.

--- MQuinnIndProp.v
+++ MQuinnIndProp.v
@@ -478,3 +478,24 @@ Proof.
   - apply H.
   - apply MStar0.
 Qed.
+
+Lemma empty_is_empty : forall T (s : list T), ~(s =~ EmptySet).
+Proof. intros. unfold not. intros H. inversion H. Qed.
+
+Lemma MUnion' : forall T (s : list T) (re1 re2 : reg_exp T),
+  s =~ re1 \/ s =~ re2 -> s =~ Union re1 re2.
+Proof.
+  intros. destruct H as [| HL HR].
+  - apply MUnionL. apply H.
+  - apply MUnionR. apply HL.
+Qed.
+
+Lemma MStar' : forall T (ss : list (list T)) (re : reg_exp T),
+  (forall s, In s ss -> s =~ re) -> fold app ss [] =~ Star re.
+Proof.
+  intros. induction ss.
+  - simpl. apply MStar0.
+  - simpl. apply MStarApp.
+    + apply H. simpl. left. reflexivity.
+    + apply IHss. intros. apply H. simpl. right. apply H0.
+Qed.
