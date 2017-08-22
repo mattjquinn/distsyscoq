74391634e5444ee1684cd1189dd24b727b24f69d
Matt Quinn <matt@mattjquinn.com>
2017-08-22 07:55:40 +0300

    Updating Ind Principles.

--- MQuinnIndPrinciples.v
+++ MQuinnIndPrinciples.v
@@ -26,3 +26,108 @@ Proof.
   - simpl. intros n' IHn'. apply n_eq_m__S_n_eq_S_m.
     apply IHn'.
 Qed.
+
+Inductive yesno : Type :=
+  | yes : yesno
+  | no : yesno.
+
+Check yesno_ind.
+
+(* rgb_ind : forall P : rgb -> Prop,
+    P red ->
+    P green ->
+    P blue ->
+    forall r : rgb, P r *)
+Inductive rgb : Type :=
+  | red : rgb
+  | green : rgb
+  | blue : rgb.
+Check rgb_ind.
+
+Inductive natlist : Type :=
+  | nnil : natlist
+  | ncons : nat -> natlist -> natlist.
+Check natlist_ind.
+
+Inductive natlist1 : Type :=
+  | nnil1 : natlist1
+  | nsnoc1 : natlist1 -> nat -> natlist1.
+
+(* natlist1_ind : forall P : natlist1 -> Prop,
+    P nnil1 ->
+    (forall (l : natlist1), P l ->
+      forall (n : nat), P (nsnoc1 l n) ->
+    forall n : natlist1, P n.
+*)
+(* Got the above almost right, except that forall (n : nat)
+   needed to be moved down instead of grouped with
+   forall (l : natlist1).
+*)
+Check natlist1_ind.
+
+Inductive byntree : Type :=
+  | bempty : byntree
+  | bleaf : yesno -> byntree
+  | nbranch : yesno -> byntree -> byntree -> byntree.
+
+(*
+  byntree_ind : forall P : byntree -> Prop,
+    P bempty ->
+    (forall y : yesno, P (bleaf y)) ->
+    (forall b1 : byntree, P b1 ->
+      (forall b2 : byntree, P b2 ->
+        forall y : yesno, P (nbranch y b1 b2))) ->
+    forall b : byntree, P b.
+*)
+Check byntree_ind.
+
+Inductive ExSet : Type :=
+  | con1 : bool -> ExSet
+  | con2 : nat -> ExSet -> ExSet.
+Check ExSet_ind.
+
+Inductive list (X:Type) : Type :=
+  | nil : list X
+  | cons : X -> list X -> list X.
+Check list_ind.
+
+Inductive tree (X : Type) : Type :=
+  | leaf : X -> tree X
+  | node : tree X -> tree X -> tree X.
+
+(*
+  tree_ind : forall (X : Type) (P : tree X -> Prop),
+    (forall x : X, P (leaf X x)) ->
+    (forall (t1 : tree X), P t1 ->
+      forall (t2 : tree X), P t2 ->
+        P (node X t1 t2)) ->
+    forall t : tree X, P t.
+*)
+Check tree_ind.
+
+Inductive mytype (X : Type) : Type :=
+  | constr1 : X -> mytype X
+  | constr2 : nat -> mytype X
+  | constr3 : mytype X -> nat -> mytype X.
+Check mytype_ind.
+
+Inductive foo (X Y : Type) : Type :=
+  | bar : X -> foo X Y
+  | baz : Y -> foo X Y
+  | quux : (nat -> foo X Y) -> foo X Y.
+Check foo_ind.
+
+Inductive foo' (X : Type) : Type :=
+  | C1 : list X -> foo' X -> foo' X
+  | C2 : foo' X.
+
+(*
+  foo'_ind : forall (X : Type) (P : foo' X -> Prop),
+    (forall (l : list X) (f : foo' X),
+      P f ->
+      P (C1 X l f) ) ->
+    P (C2 X) ->
+    forall f : foo' X, P f.
+*)
+
+Check foo'_ind.
