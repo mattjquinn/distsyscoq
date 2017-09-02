8687d1c4b8f6295573ee11cb99c03703ff473870
Matt Quinn <matt@mattjquinn.com>
2017-09-02 12:03:15 +0300

    Checkpointing at optimizer exercise.

---
+++ MQuinnImp.v
@@ -0,0 +1,176 @@
+Require Import Coq.Bool.Bool.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.omega.Omega.
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Require Import MQuinnMaps.
+
+Module AExp.
+
+Inductive aexp : Type :=
+  | ANum : nat -> aexp
+  | APlus : aexp -> aexp -> aexp
+  | AMinus : aexp -> aexp -> aexp
+  | AMult : aexp -> aexp -> aexp.
+
+Inductive bexp : Type :=
+  | BTrue : bexp
+  | BFalse : bexp
+  | BEq : aexp -> aexp -> bexp
+  | BLe : aexp -> aexp -> bexp
+  | BNot : bexp -> bexp
+  | BAnd : bexp -> bexp -> bexp.
+
+Fixpoint aeval (a : aexp) : nat :=
+  match a with
+  | ANum n => n
+  | APlus a1 a2 => (aeval a1) + (aeval a2)
+  | AMinus a1 a2 => (aeval a1) - (aeval a2)
+  | AMult a1 a2 => (aeval a1) * (aeval a2)
+  end.
+
+Example test_aeval1 : aeval (APlus (ANum 2) (ANum 2)) = 4.
+Proof. reflexivity. Qed.
+
+Fixpoint beval (b : bexp) : bool :=
+  match b with
+  | BTrue => true
+  | BFalse => false
+  | BEq a1 a2 => beq_nat (aeval a1) (aeval a2)
+  | BLe a1 a2 => leb (aeval a1) (aeval a2)
+  | BNot b1 => negb (beval b1)
+  | BAnd b1 b2 => andb (beval b1) (beval b2)
+  end.
+
+Fixpoint optimize_0plus (a : aexp) : aexp :=
+  match a with
+  | ANum n => ANum n
+  | APlus (ANum 0) e2 => optimize_0plus e2
+  | APlus e1 e2 => APlus (optimize_0plus e1) (optimize_0plus e2)
+  | AMinus e1 e2 => AMinus (optimize_0plus e1) (optimize_0plus e2)
+  | AMult e1 e2 => AMult (optimize_0plus e1) (optimize_0plus e2)
+  end.
+
+Example test_optimize_0plus : optimize_0plus
+  (APlus (ANum 2) (APlus (ANum 0) (APlus (ANum 0) (ANum 1))))
+    = APlus (ANum 2) (ANum 1).
+Proof. reflexivity. Qed.
+
+Theorem optimize_0plus_sound : forall a,
+  aeval (optimize_0plus a) = aeval a.
+Proof.
+  intros a. induction a.
+  - reflexivity.
+  - destruct a1.
+    + destruct n.
+      * simpl. apply IHa2.
+      * simpl. rewrite IHa2. reflexivity.
+    + simpl. simpl in IHa1. rewrite IHa1. rewrite IHa2. reflexivity.
+    + simpl. simpl in IHa1. rewrite IHa1. rewrite IHa2. reflexivity.
+    + simpl. simpl in IHa1. rewrite IHa1. rewrite IHa2. reflexivity.
+  - simpl. rewrite IHa1. rewrite IHa2. reflexivity.
+  - simpl. rewrite IHa1. rewrite IHa2. reflexivity.
+Qed.
+
+Theorem silly1 : forall ae, aeval ae = aeval ae.
+Proof. try reflexivity. Qed.
+
+Theorem silly2 : forall (P : Prop), P -> P.
+Proof.
+  intros P HP.
+  try reflexivity. apply HP.
+Qed.
+
+Lemma foo : forall n, leb 0 n = true.
+Proof.
+  intros. destruct n.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Lemma foo' : forall n, leb 0 n = true.
+Proof.
+  intros. destruct n; simpl; reflexivity.
+Qed.
+
+Theorem optimize_0plus_sound' : forall a,
+  aeval (optimize_0plus a) = aeval a.
+Proof.
+  intros a.
+  induction a;
+    try (simpl; rewrite IHa1; rewrite IHa2; reflexivity).
+  - reflexivity.
+  - destruct a1;
+      try (simpl; simpl in IHa1; rewrite IHa1; rewrite IHa2; reflexivity).
+    + destruct n; simpl; rewrite IHa2; reflexivity.
+Qed.
+
+Theorem optimize_0plus_sound'' : forall a,
+  aeval (optimize_0plus a) = aeval a.
+Proof.
+  intros a.
+  induction a;
+    try (simpl; rewrite IHa1; rewrite IHa2; reflexivity);
+    try reflexivity.
+  - (* The interesting case is when a = APlus a1 a2. *)
+    destruct a1; try (simpl; simpl in IHa1; rewrite IHa1;
+                      rewrite IHa2; reflexivity).
+    + (* a1 = ANum n *) destruct n;
+      simpl; rewrite IHa2; reflexivity. Qed.
+
+Theorem In10 : In 10 [1;2;3;4;5;6;7;8;9;10].
+Proof.
+  repeat (try (left; reflexivity); right).
+Qed.
+
+Theorem In10' : In 10 [1;2;3;4;5;6;7;8;9;10].
+Proof.
+  repeat (left; reflexivity).
+  repeat (right; try (left; reflexivity)).
+Qed.
+
+Fixpoint optimize_0plus_b (b : bexp) : bexp :=
+  match b with
+  | BTrue => BTrue
+  | BFalse => BFalse
+  | BEq a1 a2 => BEq (optimize_0plus a1) (optimize_0plus a2)
+  | BLe a1 a2 => BLe (optimize_0plus a1) (optimize_0plus a2)
+  | BNot b1 => BNot (optimize_0plus_b b1)
+  | BAnd b1 b2 => BAnd (optimize_0plus_b b1) (optimize_0plus_b b2)
+  end.
+
+Theorem optimize_0plus_b_sound : forall b,
+  beval (optimize_0plus_b b) = beval b.
+Proof.
+  intros. induction b;
+    repeat reflexivity;
+    repeat (simpl; rewrite (optimize_0plus_sound a);
+      rewrite (optimize_0plus_sound a0); reflexivity).
+  - simpl. rewrite IHb. reflexivity.
+  - simpl. rewrite IHb1. rewrite IHb2. reflexivity.
+Qed.
+
+Fixpoint aexp_optimizer (a : aexp) : aexp :=
+  match a with
+  | ANum n => ANum n
+  | APlus e1 e2 => APlus (aexp_optimizer e1) (aexp_optimizer e2)
+  | AMinus e1 e2 => AMinus (aexp_optimizer e1) (aexp_optimizer e2)
+  | AMult (ANum 0) e2 => ANum 0
+  | AMult e1 e2 => AMult (aexp_optimizer e1) (aexp_optimizer e2)
+  end.
+
+Theorem aexp_optimizer_sound : forall a,
+  aeval (aexp_optimizer a) = aeval a.
+Proof.
+  intros a.
+  induction a;
+    try (simpl; rewrite IHa1; rewrite IHa2; reflexivity);
+    try reflexivity.
+  - (* -> 0 * e2 ... *)
+    simpl. destruct a1;
+      repeat (simpl; simpl in IHa1; rewrite IHa1; rewrite IHa2; reflexivity).
+      simpl. destruct n;
+        simpl; try rewrite IHa2; reflexivity.
+Qed.
