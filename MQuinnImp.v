50edf67d617f413b5e857ad1a196fc897b9beef1
Matt Quinn <matt@mattjquinn.com>
2017-09-09 08:13:11 +0300

    Up to Expressions with Variables.

--- MQuinnImp.v
+++ MQuinnImp.v
@@ -305,3 +305,54 @@ Proof.
 Qed.
 
 End AExp.
+
+Module aevalR_division.
+
+Inductive aexp : Type :=
+  | ANum : nat -> aexp
+  | APlus : aexp -> aexp -> aexp
+  | AMinus : aexp -> aexp -> aexp
+  | AMult : aexp -> aexp -> aexp
+  | ADiv : aexp -> aexp -> aexp.
+
+Reserved Notation "e '\\' n" (at level 50, left associativity).
+
+Inductive aevalR : aexp -> nat -> Prop :=
+  | E_ANum : forall (n : nat), (ANum n) \\ n
+  | E_APlus : forall (a1 a2 : aexp) (n1 n2 : nat),
+    (a1 \\ n1) -> (a2 \\ n2) -> (APlus a1 a2) \\ (n1 + n2)
+  | E_AMinus : forall (a1 a2 : aexp) (n1 n2 : nat),
+    (a1 \\ n1) -> (a2 \\ n2) -> (AMinus a1 a2) \\ (n1 - n2)
+  | E_AMult : forall (a1 a2 : aexp) (n1 n2 : nat),
+    (a1 \\ n1) -> (a2 \\ n2) -> (AMult a1 a2) \\ (n1 * n2)
+  | E_ADiv : forall (a1 a2 : aexp) (n1 n2 n3 : nat),
+    (a1 \\ n1) -> (a2 \\ n2) -> (n2 > 0) ->
+    (mult n2 n3 = n1) -> (ADiv a1 a2) \\ n3
+where "e '\\' n" := (aevalR e n) : type_scope.
+
+End aevalR_division.
+
+Module aevalR_extended.
+
+Inductive aexp : Type :=
+  | AAny : aexp
+  | ANum : nat -> aexp
+  | APlus : aexp -> aexp -> aexp
+  | AMinus : aexp -> aexp -> aexp
+  | AMult : aexp -> aexp -> aexp.
+
+Reserved Notation "e '\\' n" (at level 50, left associativity).
+
+Inductive aevalR : aexp -> nat -> Prop :=
+  | E_AAny : forall (n : nat), AAny \\ n
+  | E_ANum : forall (n : nat), (ANum n) \\ n
+  | E_APlus : forall (a1 a2 : aexp) (n1 n2 : nat),
+    (a1 \\ n1) -> (a2 \\ n2) -> (APlus a1 a2) \\ (n1 + n2)
+  | E_AMinus : forall (a1 a2 : aexp) (n1 n2 : nat),
+    (a1 \\ n1) -> (a2 \\ n2) -> (AMinus a1 a2) \\ (n1 - n2)
+  | E_AMult : forall (a1 a2 : aexp) (n1 n2 : nat),
+    (a1 \\ n1) -> (a2 \\ n2) -> (AMult a1 a2) \\ (n1 * n2)
+where "e '\\' n" := (aevalR e n) : type_scope.
+
+End aevalR_extended.
+
