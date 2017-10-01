12173fec4e60ae046733c0b429a90550c4f5146a
Matt Quinn <matt@mattjquinn.com>
2017-10-01 11:11:07 +0300

    Up to Relational vs Step-Indexed Eval.

--- MQuinnImpCEvalFun.v
+++ MQuinnImpCEvalFun.v
@@ -66,3 +66,58 @@ Fixpoint ceval_step3 (st : state) (c : com) (i : nat) : option state :=
         else Some st
   end
 end.
+
+Notation "'LETOPT' x <== e1 'IN' e2"
+  := (match e1 with
+      | Some x => e2
+      | None => None
+    end)
+  (right associativity, at level 60).
+
+Fixpoint ceval_step (st : state) (c : com) (i : nat) : option state :=
+  match i with
+  | O => None
+  | S i' =>
+    match c with
+      | SKIP => Some st
+      | l ::= a1 => Some (t_update st l (aeval st a1))
+      | c1 ;; c2 => LETOPT st' <== ceval_step st c1 i' IN
+                    ceval_step st' c2 i'
+      | IFB b THEN c1 ELSE c2 FI =>
+          if (beval st b)
+            then ceval_step st c1 i'
+            else ceval_step st c2 i'
+      | WHILE b1 DO c1 END =>
+          if (beval st b1)
+          then LETOPT st' <== ceval_step st c1 i' IN
+               ceval_step st' c i'
+          else Some st
+  end
+end.
+
+Definition test_ceval (st:state) (c:com) :=
+  match ceval_step st c 500 with
+    | None => None
+    | Some st => Some (st X, st Y, st Z)
+  end.
+
+Compute
+     (test_ceval empty_state
+         (X ::= ANum 2;;
+          IFB BLe (AId X) (ANum 1)
+            THEN Y ::= ANum 3
+            ELSE Z ::= ANum 4
+          FI)).
+
+Definition pup_to_n : com :=
+  WHILE (BNot (BEq (ANum 0) (AId X))) DO
+    Y ::= (APlus (AId Y) (AId X)) ;;
+    X ::= (AMinus (AId X) (ANum 1))
+  END.
+
+Compute (test_ceval (t_update empty_state X 5) pup_to_n).
+
+Example pup_to_n_1 :
+  test_ceval (t_update empty_state X 5) pup_to_n = Some (0, 15, 0).
+Proof. reflexivity. Qed.
+
