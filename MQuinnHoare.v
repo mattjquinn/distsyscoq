6ba7a4f8053e6fcc48241bb80adea51846fb8a20
Matt Quinn <matt@mattjquinn.com>
2017-12-25 09:38:00 +0300

    Adding Hoare.

---
+++ MQuinnHoare.v
@@ -0,0 +1,8 @@
+Set Warnings "-notation-overridden,-parsing".
+Require Import Coq.Bool.Bool.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.omega.Omega.
+Require Import MQuinnImp.
+Require Import MQuinnMaps.
+
