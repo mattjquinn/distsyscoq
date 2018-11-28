529f7e2153b28f56f7ace0c88526260389f11776
Matt Quinn <matt@mattjquinn.com>
2018-11-28 09:59:07 +0300

    Checkpoint

--- dualprocess/DualProcess.v
+++ dualprocess/DualProcess.v
@@ -1,36 +1,41 @@
 Require Import List.
 Require Import Nat.
 Require Import String.
+Require Import Omega.
 
 Delimit Scope string_scope with string.
 Local Open Scope string_scope.
 Local Open Scope list_scope.
 
 Definition channel : Type := list string.
-
-Inductive node : Type :=
-| Node : nat -> node.
-
-Definition lvar (n : node) : nat := match n with | Node lv => lv end.
-
+Record netstate := NetState {t : nat; r : nat; c1 : channel; c2 : channel}.
 Definition msg : string := " ""Message."" ".
+Definition initial := {| t := 5 ; r := 5 ; c1 := nil ; c2 := nil |}.
 
-Inductive netEvalR : node -> node -> channel -> channel -> Prop :=
-| Step1 : forall n1 n2 c1 c2,
-    lvar n1 > 0 ->
-    (netEvalR (Node (lvar n1 - 1)) n2 (msg :: c1) c2)
-| Step2 : forall n1 n2 c1 c2,
-    c2 <> nil ->
-    (netEvalR (Node (lvar n1 + 1)) n2 c1 (tl c2))
+Inductive netEvalR : netstate -> netstate -> Prop :=
+| Initial : (netEvalR initial initial)
+| Step1 : forall st,
+    st.(t) > 0 ->
+    (netEvalR st {| t := st.(t) - 1 ; r := st.(r) ;
+                    c1 := msg :: st.(c1) ; c2 := st.(c2) |})
+| Step2 : forall st,
+    st.(c2) <> nil ->
+    (netEvalR st {| t := st.(t) + 1 ; r := st.(r) ;
+                    c1 := st.(c1) ; c2 := (tl st.(c2)) |}).
+(*
 | Step3 : forall n1 n2 c1 c2,
     c1 <> nil ->
     (netEvalR n1 (Node (lvar n2 + 1)) (tl c1) c2)
 | Step4 : forall n1 n2 c1 c2,
     lvar n2 > 0 ->
     (netEvalR n1 (Node (lvar n2 - 1)) c1 (msg :: c2)).
+*)
 
-Definition T : node := Node 5.
-Definition R : node := Node 5.
-Definition c1 : channel := nil.
-Definition c2 : channel := nil.
-
+Theorem safety_holds: forall st st' : netstate,
+         (netEvalR st st') ->
+         st'.(t) >= 0 /\ st'.(r) >= 0.
+Proof.
+  intros st st' H. split.
+  - inversion H. omega. omega. omega.
+  - inversion H. omega. omega. omega.
+Qed.
